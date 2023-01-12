69d5048994e64b63939c0a071fb58b617b095883
mListek <michal.listex@gmail.com>
2023-01-12 09:32:56 +0100

    changed App to functional compnent

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to the library</h1>
+    <div>
+      <Navbar />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
-
-export default App;
