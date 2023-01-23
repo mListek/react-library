ee8b9f4ae308b62538372e63d25aa434a1b5d876
mListek <michal.listex@gmail.com>
2023-01-23 19:28:37 +0100

    implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
-}
+};
